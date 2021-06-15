750c4af8f60d2401a32092f51dd06c564881b980
Zachery Van Es <47612428+ZacheryThomasVanEs@users.noreply.github.com>
2021-06-14 19:21:41 -0700

    Add files via upload

---
+++ Fizzbuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                bool fizz = (i % 3 == 0);
+                bool buzz = (i % 5 == 0);
+
+                if (fizz && buzz)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+                else if (fizz)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (buzz)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
